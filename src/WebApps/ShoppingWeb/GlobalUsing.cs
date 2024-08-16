bd1b7bd1657b9110d5941ed3ce0041bc98b22d96
Alamin <amialaminb@gmail.com>
2024-08-16 20:11:00 +0600

    little bit left

---
+++ src/WebApps/ShoppingWeb/GlobalUsing.cs
@@ -0,0 +1,7 @@
+﻿global using ShoppingWeb.Models.Catalog;
+global using Refit;
+global using ShoppingWeb.Services;
+global using ShoppingWeb.Models.Basket;
+global using ShoppingWeb.Models.Ordering;
+global using Microsoft.AspNetCore.Mvc.RazorPages;
+global using Microsoft.AspNetCore.Mvc;
